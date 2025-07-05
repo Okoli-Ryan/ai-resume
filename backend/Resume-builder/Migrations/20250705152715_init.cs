15b5a0bad260ea613bcc7b982627e2c9b668fe97
okoli-ryan <okoliryan50@gmail.com>
2025-07-05 16:28:52 +0100

    Initial migration

---
+++ backend/Resume-builder/Migrations/20250705152715_init.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Resume_builder.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: true),
+                    GoogleId = table.Column<string>(type: "text", nullable: true),
+                    ActiveStatus = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Resume",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    ResumeName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    UserFullName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    UserEmail = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: true),
+                    TextSummary = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: true),
+                    JobRole = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    IsFavourite = table.Column<bool>(type: "boolean", nullable: false),
+                    UserAddress = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    UserPhoneNumber = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    LinkedinUrl = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    GithubUrl = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    PortfolioUrl = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Tags = table.Column<string>(type: "text", nullable: true),
+                    Order = table.Column<string>(type: "text", nullable: true),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    ActiveStatus = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resume", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Resume_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Education",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    SchoolName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Degree = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    FieldOfStudy = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Location = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    IsOngoing = table.Column<bool>(type: "boolean", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ResumeId = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    ActiveStatus = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Education", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Education_Resume_ResumeId",
+                        column: x => x.ResumeId,
+                        principalTable: "Resume",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Education_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    ProjectName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    ProjectUrl = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    ResumeId = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    ActiveStatus = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Project_Resume_ResumeId",
+                        column: x => x.ResumeId,
+                        principalTable: "Resume",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Project_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Skill",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Group = table.Column<string>(type: "text", nullable: false),
+                    Skills = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    ResumeId = table.Column<string>(type: "text", nullable: false),
+                    ActiveStatus = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skill", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Skill_Resume_ResumeId",
+                        column: x => x.ResumeId,
+                        principalTable: "Resume",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Skill_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkExperience",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    ResumeId = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    CompanyName = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: true),
+                    CompanyLink = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: true),
+                    Title = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: true),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsOngoing = table.Column<bool>(type: "boolean", nullable: false),
+                    Location = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: true),
+                    WorkType = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: true),
+                    ActiveStatus = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkExperience", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkExperience_Resume_ResumeId",
+                        column: x => x.ResumeId,
+                        principalTable: "Resume",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WorkExperience_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BulletPoint",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Text = table.Column<string>(type: "text", nullable: false),
+                    Order = table.Column<int>(type: "integer", nullable: false),
+                    EducationId = table.Column<string>(type: "text", nullable: true),
+                    ProjectId = table.Column<string>(type: "text", nullable: true),
+                    WorkExperienceId = table.Column<string>(type: "text", nullable: true),
+                    ActiveStatus = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BulletPoint", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BulletPoint_Education_EducationId",
+                        column: x => x.EducationId,
+                        principalTable: "Education",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_BulletPoint_Project_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Project",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_BulletPoint_WorkExperience_WorkExperienceId",
+                        column: x => x.WorkExperienceId,
+                        principalTable: "WorkExperience",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BulletPoint_EducationId",
+                table: "BulletPoint",
+                column: "EducationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BulletPoint_ProjectId",
+                table: "BulletPoint",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BulletPoint_WorkExperienceId",
+                table: "BulletPoint",
+                column: "WorkExperienceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Education_ResumeId",
+                table: "Education",
+                column: "ResumeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Education_UserId",
+                table: "Education",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_ResumeId",
+                table: "Project",
+                column: "ResumeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_UserId",
+                table: "Project",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resume_UserId",
+                table: "Resume",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skill_ResumeId",
+                table: "Skill",
+                column: "ResumeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skill_UserId",
+                table: "Skill",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkExperience_ResumeId",
+                table: "WorkExperience",
+                column: "ResumeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkExperience_UserId",
+                table: "WorkExperience",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BulletPoint");
+
+            migrationBuilder.DropTable(
+                name: "Skill");
+
+            migrationBuilder.DropTable(
+                name: "Education");
+
+            migrationBuilder.DropTable(
+                name: "Project");
+
+            migrationBuilder.DropTable(
+                name: "WorkExperience");
+
+            migrationBuilder.DropTable(
+                name: "Resume");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
